23a9e9155094105f0c4c11a33c1a8ea535348a68
EC2 Default User <ec2-user@ip-172-31-30-156.us-east-2.compute.internal>
2018-04-06 03:58:06 +0000

    fix login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,16 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all.page(params[:page])
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def new
-    @task = Task.new
-  end
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
-    end
-  end
-
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  def update
-    @task = Task.find(params[:id])
-    
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
-    else 
-      flash.now[:danger] = 'Taskは更新されませんでした'
-      render :edit
+      render 'toppages/index'
     end
   end
 
@@ -44,7 +19,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
@@ -52,6 +27,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
